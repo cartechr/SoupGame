f3c656a9c78955003c5f5ed8ec3f4d2b2be5c4cf
Christian L Carter <54558708+cartechr@users.noreply.github.com>
2025-01-22 18:18:59 -0600

    Interactible

---
+++ SoupUnityGame/Assets/Scripts/Interactible.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactible : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent OnInteract;
+    public void Interact()
+    {
+        OnInteract.Invoke();
+    }
+
+
+}
--- SoupUnityGame/Assets/Scripts/PlayerInteraction.cs
+++ SoupUnityGame/Assets/Scripts/PlayerInteraction.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<Interactible> interatibles = new List<Interactible>();
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
-    }
 
+    }
     public void ReceiveInteractInput()
     {
-        Debug.Log("interacted!!");
+        if (interatibles.Count == 0)
+            return;
+
+        float shortestDistance = Mathf.Infinity;
+        Interactible savedInteractible = null;
+
+        foreach (Interactible item in interatibles) 
+        {
+            float distance = Vector3.Distance(transform.position, item.transform.position);
+            if(distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                savedInteractible = item;
+            }
+        }
+        savedInteractible.Interact();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Interactible>() == true)
+        {
+            interatibles.Add(collision.GetComponent<Interactible>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Interactible>() == true)
+        {
+            interatibles.Remove(collision.GetComponent<Interactible>());
+        }
     }
 }
